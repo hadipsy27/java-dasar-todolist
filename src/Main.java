6410383c9b5f51d08fc77a88debcc6131baa3b6e
hadipsy27 <hadipsy27@gmail.com>
2023-07-02 12:32:45 +0700

    Final Test keseluruhan applikasi

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        testViewRemoveTodoList();
+        viewShowTodoList();
     }
 
     /**
